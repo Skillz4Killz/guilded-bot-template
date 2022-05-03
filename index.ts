c5ab958b2fa429c56ab308cf31e32f93e348d289
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-03 15:14:30 +0000

    feat: base file to start bot

---
+++ index.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { BotClient } from "@guildedjs/gil";
+import { TOKEN } from "../configs";
+
+const client = new BotClient({
+  token: TOKEN,
+  prefix: "!",
+});
+
+client.login();
+
+process.on("unhandledRejection", console.log);
